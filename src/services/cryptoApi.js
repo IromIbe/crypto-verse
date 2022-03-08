a88598e848b95eea5de81e936cfd7349c85f830d
iromIbe <IromIbe@outlook.com>
2022-03-08 14:47:32 +0100

    populated website with data from coin ranking api

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,39 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "fcebe6803dmsh715b8722a8a438ap1b4dddjsnf2dc018a9829",
+};
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
+
+// var options = {
+//   method: "GET",
+//
+//   params: {
+//     referenceCurrencyUuid: "yhjMzLPhuIDl",
+//     timePeriod: "24h",
+//     tiers: "1",
+//     orderBy: "marketCap",
+//     orderDirection: "desc",
+//     limit: "50",
+//     offset: "0",
+//   },
+//   headers: {
+//     "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+//     "x-rapidapi-key": "fcebe6803dmsh715b8722a8a438ap1b4dddjsnf2dc018a9829",
+//   },
+// };
