6ea0db457875257738ee8d20043ec194b5053691
iromIbe <IromIbe@outlook.com>
2022-03-29 00:28:12 +0100

    cleard unused variables

--- src/component/exchange/Exchanges.js
+++ src/component/exchange/Exchanges.js
@@ -1,6 +1,6 @@
 import React from "react";
 // import millify from "millify";
-// import { Row, Col } from "antd";
+import { Row, Col } from "antd";
 // import HTMLReactParser from "html-react-parser";
 
 import { useGetExchangesQuery } from "../../services/cryptoApi";
