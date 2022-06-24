889d6e675af5577b278d1ce74dc9d0d9738e3c4a
IromIbe <iromibe@outlook.com>
2022-06-24 18:10:24 +0100

    make it better

--- src/component/homepage/Homepage.js
+++ src/component/homepage/Homepage.js
@@ -11,73 +11,77 @@ const Homepage = () => {
   const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
-  if (isFetching) return <Loader />;
-  //  console.log(data, 'Hey Coins')
   return (
     <>
-      <Title level={2} className='heading'>
-        Global Crypto Stats
-      </Title>
-      <Row>
-        <Col span={12}>
-          <Statistic
-            title='Total Cryprocurrencies'
-            value={globalStats?.total}
-          />
-        </Col>
-        {globalStats?.totalExchanges && (
-          <Col span={12}>
-            <Statistic
-              title='Total Exchanges'
-              value={globalStats?.totalExchanges}
-            />
-          </Col>
-        )}
-        {globalStats?.totalMarketCap && (
-          <Col span={12}>
-            <Statistic
-              title='Total Market Cap'
-              value={millify(globalStats?.totalMarketCap)}
-            />
-          </Col>
-        )}
-        {globalStats?.total24hVolume && (
-          <Col span={12}>
-            <Statistic
-              title='Total 24th Volume'
-              value={millify(globalStats?.total24hVolume)}
-            />
-          </Col>
-        )}
-        {globalStats?.totalMarkets && (
-          <Col span={12}>
-            <Statistic
-              title='Total Markets'
-              value={millify(globalStats?.totalMarkets)}
-            />
-          </Col>
-        )}
-      </Row>
-      <div className='home-heading-container'>
-        <Title level={2} className='home-title'>
-          Top 10 Cryptocurrencies in the World
-        </Title>
-        <Title level={3} className='show-more'>
-          {" "}
-          <Link to='/cryptocurrencies'>Show More</Link>
-        </Title>
-      </div>
-      <Cryptocurrencies simplified />
-      <div className='home-heading-container'>
-        <Title level={2} className='home-title'>
-          Latest Crypto News
-        </Title>
-        <Title level={3} className='show-more'>
-          {" "}
-          <Link to='/news'>Show More</Link>
-        </Title>
-      </div>
-      <News simplified />
+      {isFetching ? (
+        <Loader />
+      ) : (
+        <>
+          <Title level={2} className='heading'>
+            Global Crypto Stats
+          </Title>
+          <Row>
+            <Col span={12}>
+              <Statistic
+                title='Total Cryprocurrencies'
+                value={globalStats?.total}
+              />
+            </Col>
+            {globalStats?.totalExchanges && (
+              <Col span={12}>
+                <Statistic
+                  title='Total Exchanges'
+                  value={globalStats?.totalExchanges}
+                />
+              </Col>
+            )}
+            {globalStats?.totalMarketCap && (
+              <Col span={12}>
+                <Statistic
+                  title='Total Market Cap'
+                  value={millify(globalStats?.totalMarketCap)}
+                />
+              </Col>
+            )}
+            {globalStats?.total24hVolume && (
+              <Col span={12}>
+                <Statistic
+                  title='Total 24th Volume'
+                  value={millify(globalStats?.total24hVolume)}
+                />
+              </Col>
+            )}
+            {globalStats?.totalMarkets && (
+              <Col span={12}>
+                <Statistic
+                  title='Total Markets'
+                  value={millify(globalStats?.totalMarkets)}
+                />
+              </Col>
+            )}
+          </Row>
+          <div className='home-heading-container'>
+            <Title level={2} className='home-title'>
+              Top 10 Cryptocurrencies in the World
+            </Title>
+            <Title level={3} className='show-more'>
+              {" "}
+              <Link to='/cryptocurrencies'>Show More</Link>
+            </Title>
+          </div>
+          <Cryptocurrencies simplified />
+          <div className='home-heading-container'>
+            <Title level={2} className='home-title'>
+              Latest Crypto News
+            </Title>
+            <Title level={3} className='show-more'>
+              {" "}
+              <Link to='/news'>Show More</Link>
+            </Title>
+          </div>
+          <News simplified />
+        </>
+      )}
     </>
   );
 };
